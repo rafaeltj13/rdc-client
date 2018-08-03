6f69501e0b9d3e2445b01e374b75beb46845181d
Rafael <rafael.maciel@ccc.ufcg.edu.br>
2018-08-02 21:29:46 -0300

    Adding folders

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Sobre from './Sobre';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/" exact={true} component={App} />
+            <Route path="/sobre" component={Sobre} />
+        </Switch>
+    </BrowserRouter>
+    , document.getElementById('root'));
 registerServiceWorker();
